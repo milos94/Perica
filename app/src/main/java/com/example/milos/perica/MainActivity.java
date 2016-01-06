7f332c8205478a4f54281a18805c806991518774
milos94 <a.milos94@gmail.com>
2016-01-06 14:34:58 +0100

    06.01.2016.
    -Najzad je odrađeno štampanje liste preko Messeg-a i Handler-a.
    -Promenjeno je da klijent šalje stringove umesto objekata zbog komandi
    copy i move.
    -Dodati su stringovi cache, koji se korisit za copy i move, pamti se šta
    se kopira, i pozicija, koji pamti gde se nalazimo.
    -Šalju se stringovi umesto objekata zbog copy i move.
    -Dodat je pomoćni meni, koji bi trebao da se pojavi kad se dugo klikne
    na nekšto iz liste, sa opcijama Otvori, Obrisi, Kopiraj, Iseci i Nalepi,
    mad još uvek nije namešteno da se pojavljuje.
    Srećni praznici!

--- app/src/main/java/com/example/milos/perica/MainActivity.java
+++ app/src/main/java/com/example/milos/perica/MainActivity.java
@@ -5,16 +5,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -25,40 +33,60 @@ import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import android.widget.AbsListView.MultiChoiceModeListener;
 
 import models.Konekcija;
 import models.Poruka;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static List<Poruka> lista= new ArrayList<Poruka>();
+    private Handler handler;
     Konekcija konekcija;
     ListView listView;
     ArrayAdapter<Poruka> adapter;
-
-
+    String cache,pozicija;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adapter=new ArrayAdapter<Poruka>(getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                MainActivity.lista);
+        cache=pozicija="";
+        inicjalizacijaListe();
+        handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                switch (msg.what) {
+                    case 1:
+                        ArrayList<Poruka> listaPoruka = (ArrayList<Poruka>) msg.obj;
+                        adapter = new ArrayAdapter<Poruka>(getApplicationContext(),
+                            android.R.layout.simple_list_item_1,
+                            listaPoruka);
+                        listView.setAdapter(adapter);
+                        break;
+
+                    case 2:
+                        konekcija=null;
+                        //treba da se doda obavestenje da je prekinuta veza sa serverom
+                        break;
+
+                    default:
+                    super.handleMessage(msg);
+                        break;
+                }
+            }
+        };
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        konekcija=null;
-        listView=(ListView) findViewById(R.id.list1);
         setSupportActionBar(toolbar);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-    public  void StampajListu(Context ctx){
 
 
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -67,51 +95,91 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
            Intent intent= new Intent(this,Podesavanja.class);
            startActivity(intent);
         }
         if(id==R.id.paljeje) {
-
             if(konekcija==null) {
-               // MainActivity.this.runOnUiThread(new Konekcija(getApplicationContext(),listView));
-                //konekcija= new Konekcija(getApplicationContext(),listView);
-                new MojaKlasa().execute();
-                //konekcija.start();
-                return true;
+                konekcija = new Konekcija(this, handler);
+                konekcija.start();
+                //treba da se doda obavestenje da je povezano sa serverom
             }
             else{
-                try {
-                    konekcija.kraj();
-                    konekcija=null;
-                }catch (IOException e){e.printStackTrace();}
-                catch (Exception e){e.printStackTrace();}
+                konekcija.posaljiPoruku("SHUTDOWN");
             }
+
         }
 
         return super.onOptionsItemSelected(item);
     }
-    
-    class MojaKlasa extends  AsyncTask<Void, Poruka, Poruka>{
-        private Konekcija kon;
-        private ArrayAdapter<Poruka> adapter;
-        @Override
-        protected void onPreExecute(){
-            adapter=(ArrayAdapter<Poruka>)listView.getAdapter();
-            kon=new Konekcija(getApplicationContext());
-            kon.start();
-        }
-        @Override
-        protected Poruka doInBackground(Void... params) {
-            while(true){
-                kon.uzmi();
-                onProgressUpdate(kon.uzmi());
-            }
-        }
 
-        protected void onProgressUpdate(Poruka params){
-            adapter.add(params);
+    private void inicjalizacijaListe(){
+        listView=(ListView) findViewById(R.id.listView);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                if(konekcija==null) return;
+                konekcija.posaljiPoruku(adapter.getItem((int) id).toString());
+                obradaFajla(adapter.getItem(position));
+                int a=1;
+                int b=1;
+                int c=1;
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                getMenuInflater().inflate(R.menu.pomocni_meni, menu);
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Obrisi:
+                        konekcija.posaljiPoruku("rm ");
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(konekcija==null) return;
+                obradaFajla(adapter.getItem(position));
+            }
+        });
+    }
+
+
+    //Obrada izabranog fajla iz liste
+    private void obradaFajla(Poruka poruka){
+        if(poruka.getFajl().equals("..")){
+            String[] niz=pozicija.split(String.valueOf('\\'));
+            pozicija="";
+            for(int i=0;i<niz.length-1;i++){
+                pozicija+=niz[i]+"\\";
+            }
+            return;
+        }
+        switch (poruka.getEkstenzija()){
+            case ".txt": konekcija.posaljiPoruku("edit "+pozicija+'\\'+poruka.toString()); return;
+            case "":pozicija+='\\'+poruka.toString(); konekcija.posaljiPoruku("dir "+pozicija); return;
+            case ".exe": konekcija.posaljiPoruku("run "+pozicija+'\\'+poruka.toString()); return;
+            case ".jpg":case ".jpeg":case ".png":case ".bpn": konekcija.posaljiPoruku(pozicija+'\\'+poruka.toString()); return;
+            default: konekcija.posaljiPoruku(pozicija+'\\'+poruka.toString()); return;
         }
     }
 }
--- app/src/main/java/models/Konekcija.java
+++ app/src/main/java/models/Konekcija.java
@@ -2,6 +2,8 @@ package models;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -17,61 +19,63 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import android.os.Handler;
 
 /**
  * Created by Milos on 21/12/2015.
  */
 public class Konekcija extends Thread{
+
     Socket socket;
     ObjectInputStream inputStream;
     ObjectOutputStream outputStream;
+
     volatile boolean radi;
     List<Poruka> lista;
     Context ctx;
-    ArrayAdapter<Poruka> adapter;
-    ListView listView;
+    Message msg;
+    Handler handler;
 
-
-    public Konekcija(Context ct){
+    public Konekcija(Context ct, Handler hand){
         lista=new ArrayList<Poruka>();
-        radi=true;
+        radi=false;
         ctx=ct;
-        this.listView=listView;
+        this.handler=hand;
     }
 
 
 
     public void run(){
         try{
-            socket = new Socket(Razmena.getDefaults("IP", ctx),
-                    Integer.parseInt(Razmena.getDefaults("PORT", ctx)));
-            inputStream= new ObjectInputStream(socket.getInputStream());
-            outputStream=new ObjectOutputStream(socket.getOutputStream());
-           /* while(radi){
-                Poruka p= new Poruka();
-                p=(Poruka) inputStream.readObject();
-                if(p.getFajl().equals("START")){ lista.clear();}
+            pokreni();
+            while(radi){
+                Poruka p= (Poruka) inputStream.readObject();
+                if(p.getFajl().equals("START")){ lista.clear(); continue;}
                 else if(p.getFajl().equals("END")){
-                    ArrayAdapter<Poruka> adapter=new ArrayAdapter<Poruka>(ctx,
-                            android.R.layout.simple_list_item_1,
-                            lista);
-                    listView.setAdapter(adapter);
-                    //Treba da se odstampa lista...
+                    msg=Message.obtain();
+                    msg.what=1;
+                    msg.obj=lista;
+                    handler.sendMessage(msg);
+                    continue;
                 }
                 else if(p.getFajl().equals("SHUTDOWN")){
                     kraj();
+                    msg=Message.obtain();
+                    msg.what=2;
+                    handler.sendMessage(msg);
                     break;}
                 else{ lista.add(p);}
-            }*/
+            }
         }catch (IOException e){ e.printStackTrace();}
         catch (Exception e){e.printStackTrace();}
     }
 
 
 
-    public void posaljiPoruku(Poruka p){
+    public void posaljiPoruku(String p){
         try {
             outputStream.writeObject(p);
+            outputStream.flush();
         }catch (IOException e){e.printStackTrace();}
         catch (Exception e){e.printStackTrace();}
     }
@@ -86,16 +90,17 @@ public class Konekcija extends Thread{
     }
 
 
-    public Poruka uzmi() {
-        try {
-            return (Poruka) inputStream.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public void pokreni() {
+        radi=true;
+        try{
+            socket=new Socket(Razmena.getDefaults("IP",ctx),
+                    Integer.parseInt(Razmena.getDefaults("PORT", ctx)));
+            inputStream=new ObjectInputStream(socket.getInputStream());
+            outputStream=new ObjectOutputStream(socket.getOutputStream());
+        }catch (IOException e){e.printStackTrace();}
+    }
+
+    public boolean getRadi(){
+        return radi;
     }
 }
